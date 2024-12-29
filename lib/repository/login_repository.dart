fcbc5dc43da43a1d4621159757c5a4c01042237e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-29 16:27:05 +0600

    url including done

---
+++ lib/repository/login_repository.dart
@@ -0,0 +1,11 @@
+import 'package:mvvm_architucture/data/network/network_api_services.dart';
+import 'package:mvvm_architucture/res/app_url/app_url.dart';
+
+class LoginRepository {
+  final _apiService = NetworkApiServices();
+
+  Future<dynamic> loginApi(var data) async {
+    dynamic response = _apiService.postApi(data, AppUrl.loginApi);
+    return response;
+  }
+}
