f841293804c6213ea4d760ec362144f80a05a5e6
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-29 11:58:40 +0600

    add complete

---
+++ lib/utils/utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mvvm_architucture/res/colors/app_color.dart';
+
+class Utils {
+  static void fieldFocusChange(
+      BuildContext context, FocusNode current, FocusNode nextFocus) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: AppColor.blackColor,
+      gravity: ToastGravity.CENTER,
+    );
+  }
+
+  static snackBar(String title, String message) {
+    Get.snackbar(title, message);
+  }
+}
