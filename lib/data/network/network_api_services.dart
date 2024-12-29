f6421cfa069a5d671bcf28a68b1ae59cf6618dfb
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-29 13:17:13 +0600

    add complete

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:mvvm_architucture/data/app_exception.dart';
 import 'package:mvvm_architucture/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -9,10 +10,32 @@ import 'package:http/http.dart' as http;
 class NetworkApiServices extends BaseApiServices {
   @override
   Future<dynamic> getApi(String url) async {
+    if (kDebugMode) {
+      print(url);
+    }
     dynamic responseJson;
     try {
       final response =
           await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw InternetException('');
+    } on TimeoutException {
+      throw RequestTimeOut('');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future<dynamic> postApi(var data, String url) async {
+    if (kDebugMode) {
+      print(data);
+    }
+    dynamic responseJson;
+    try {
+      final response = await http
+          .post(Uri.parse(url), body: jsonEncode(data))
+          .timeout(const Duration(seconds: 10));
       responseJson = returnResponse(response);
     } on SocketException {
       throw InternetException('');
@@ -34,10 +57,5 @@ class NetworkApiServices extends BaseApiServices {
         throw FetchDataException(
             'Error occurred while communication with server${response.statusCode.toString()}');
     }
-  }
-
-  @override
-  Future postApi(String url) {
-    throw UnimplementedError();
   }
 }
